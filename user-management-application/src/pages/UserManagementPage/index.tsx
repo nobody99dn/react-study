c05c8aedb4c2afc170af530b18bc583e00ac3bca
Minh Hoang Nguyen <minh.nguyenhoang@asnet.com.vn>
2021-10-15 10:36:04 +0700

    button component commit

--- user-management-application/src/pages/UserManagementPage/index.tsx
+++ user-management-application/src/pages/UserManagementPage/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Button } from "../../components/Button";
 var faker = require("faker");
 
 export default function UserManagementPage() {
-  return <div>{faker.name.findName()}</div>;
+  return (
+    <div>
+      {faker.name.findName()}
+      <Button children={"Add New User"}></Button>
+    </div>
+  );
 }
