698af4eb235d6b17fa4f73626b9eada8f215a401
Van Tran <van.tran@asnet.com.vn>
2022-03-16 16:50:15 +0700

    Improve structure

---
+++ todos-app/src/index.js
@@ -0,0 +1,5 @@
+import Controller from './scripts/controller/controller'
+import Model from './scripts/models/model'
+import View from './scripts/view/view'
+
+new Controller(new Model(), new View())
--- todos-app/src/js/app/app.js
+++
@@ -1,7 +0,0 @@
-import Controller from '../controllers/controller'
-import Model from '../models/model'
-import View from '../views/view'
-
-const app = new Controller(new Model(), new View())
-
-app()
