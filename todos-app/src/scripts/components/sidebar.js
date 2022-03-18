c82680805c0c5a4298bd92f34f8842c7b54b5de2
Van Tran <van.tran@asnet.com.vn>
2022-03-18 15:19:40 +0700

    Implemet add new group button and update UI

--- todos-app/src/scripts/components/sidebar.js
+++ todos-app/src/scripts/components/sidebar.js
@@ -1,4 +1,4 @@
-import logo from '../../assets/images/todo-icon.png';
+import logo from '../../assets/images/todo-logo.png';
 
 /**
  * Sidebar() returns a new element
@@ -6,15 +6,20 @@ import logo from '../../assets/images/todo-icon.png';
  */
 const Sidebar = () => `
   <div class="sidebar position-relative d-flex flex-column vh-100 p-1">
-    <div>
-      <a class="logo-container m-auto" href="#"><img class="w-100" src="${logo}" alt="Icon" /></a>
+    <div class="text-center">
+      <a class="logo-container" href="#"><img class="w-75" src="${logo}" alt="Icon" /></a>
       <h1 class="sidebar-title text-center">Todo Application</h1>
     </div>
     <div class="list-item-container"></div>
       <div class="add-new-container position-absolute bottom-0">
         <div class="w-25 d-inline new-list-container"></div>
-        <div class="w-75 d-inline new-group-container"></div>
+        <div class="w-75 d-inline new-group-container">
+          <button class="btn py-1 px-2">
+            <i class="fa-solid fa-folder-plus fa-lg"></i>
+          </button>
+        </div>
       </div>
+      <hr>
   </div>
 `;
 export default Sidebar;
