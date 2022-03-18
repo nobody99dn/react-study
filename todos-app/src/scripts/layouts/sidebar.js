fe48aff043c6597d9a6262e8888134f32ed1009f
Van Tran <van.tran@asnet.com.vn>
2022-03-18 18:04:19 +0700

    Update icon

--- todos-app/src/scripts/layouts/sidebar.js
+++ todos-app/src/scripts/layouts/sidebar.js
@@ -14,11 +14,8 @@ const Sidebar = () => `
       <div class="add-new-container position-absolute bottom-0">
         <div class="w-25 d-inline new-list-container"></div>
         <div class="w-75 d-inline new-group-container">
-          <button class="btn py-1 px-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
-            <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
-            <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
-          </svg>
+          <button class="btn px-2">
+          <i class="bi bi-folder-plus fs-2"></i>
         </button>
         </div>
       </div>
