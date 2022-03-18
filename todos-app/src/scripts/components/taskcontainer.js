283bfa825c7fbda5aae5adb7b42532e32ee57abd
Bao Gia Thai <bao.thai@asnet.com.vn>
2022-03-18 17:08:49 +0700

    Fix task container

--- todos-app/src/scripts/components/taskcontainer.js
+++ todos-app/src/scripts/components/taskcontainer.js
@@ -1,17 +1,13 @@
-const TasksContainer = () => `
+const TasksContainer = (titleContent = '') => `
   <div class="task-container vh100 p-5">
-    <h1>My Todo list</h1>
-    <div class="task-subtitle fs-6 fst-italic">18 March 2022</div>
-    <div class="task-list mt-4">
-      <div class="task-detail rounded bg-secondary text-white rounded p-3 mt-1">
-        <div class="form-check">
-          <input class="task-checkbox form-check-input" type="checkbox" value="">
-          <label class="form-check-label">
-            <p>This is content</p>
-          </label>
-        </div>
-      </div>
+    <div class="title row">  
+      <h1 class="col-md-8">${titleContent}</h1>
+      <div class="col-md-4"></div>
     </div>
-`
+    <div class="task-date task-subtitle fs-6 fst-italic"></div>
+    <div class="task-list"></div>
+    <div class="task-detail"></div>
+  </div>
+`;
 
-export default TasksContainer
+export default TasksContainer;
