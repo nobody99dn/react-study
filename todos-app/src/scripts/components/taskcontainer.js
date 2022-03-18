4f51218270fb033c15576e400f7e8b300cc84d91
Bao Gia Thai <bao.thai@asnet.com.vn>
2022-03-18 16:35:01 +0700

    Fix css code and task container

--- todos-app/src/scripts/components/taskcontainer.js
+++ todos-app/src/scripts/components/taskcontainer.js
@@ -1,9 +1,9 @@
 const TasksContainer = () => `
-  <div class = "task-container vh100 p-5">
-    <div class="task-title"><h1>My Todo list</h1></div>
+  <div class="task-container vh100 p-5">
+    <h1>My Todo list</h1>
     <div class="task-subtitle fs-6 fst-italic">18 March 2022</div>
-    <div class=task-list>
-      <div class="task-detail rounded bg-secondary text-white rounded p-3">
+    <div class="task-list mt-4">
+      <div class="task-detail rounded bg-secondary text-white rounded p-3 mt-1">
         <div class="form-check">
           <input class="task-checkbox form-check-input" type="checkbox" value="">
           <label class="form-check-label">
