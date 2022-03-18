ffb3a90fa37f1ff8ee87b86fcff04d768cecbf26
Van Tran <van.tran@asnet.com.vn>
2022-03-18 11:52:35 +0700

    Update sidebar component

--- todos-app/src/index.js
+++ todos-app/src/index.js
@@ -1,7 +1,7 @@
-import Controller from './scripts/controllers/controller'
-import Model from './scripts/models/model'
-import View from './scripts/views/view'
+import Controller from './scripts/controllers/controller';
+import Model from './scripts/models/model';
+import View from './scripts/views/view';
 
-const app = new Controller(new Model(), new View())
+const app = new Controller(new Model(), new View());
 
-app()
+app();
