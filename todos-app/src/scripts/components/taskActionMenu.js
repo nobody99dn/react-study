485225fd22213278df6e2c5159e7698da4f590cd
bao.thai <bao.thai@asnet.com.vn>
2022-03-21 10:49:00 +0700

    Implement task action menu component

---
+++ todos-app/src/scripts/components/taskActionMenu.js
@@ -0,0 +1,9 @@
+const tasksActionMenu = () => `
+  <div class="task-action-menu">
+    <div class="menu-item">Add</div>
+    <div class="menu-item">Edit</div>
+    <div class="menu-item">Delete</div>
+  </div>
+`;
+
+export default tasksActionMenu;
