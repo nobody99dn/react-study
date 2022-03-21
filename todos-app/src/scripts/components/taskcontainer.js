5cc61b1607776cb4ec37a8b0b66195e14b8e2040
bao.thai <bao.thai@asnet.com.vn>
2022-03-21 08:59:45 +0700

    Change file name

--- todos-app/src/scripts/components/taskcontainer.js
+++ todos-app/src/scripts/components/taskcontainer.js
@@ -1,7 +1,7 @@
 const TasksContainer = (titleContent = '') => `
   <div class="task-container vh100 p-5">
     <div class="title row">  
-      <h1 class="col-md-8">${titleContent}</h1>
+      <div class="col-md-8">${titleContent}</div>
       <div class="col-md-4"></div>
     </div>
     <div class="task-date task-subtitle fs-6 fst-italic"></div>
