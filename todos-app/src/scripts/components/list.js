c4169faeb2eb11aa2dee6de5666b707401331436
Van Tran <van.tran@asnet.com.vn>
2022-03-21 22:18:30 +0700

    Update UI for group and list component

--- todos-app/src/scripts/components/list.js
+++ todos-app/src/scripts/components/list.js
@@ -5,10 +5,16 @@
 const List = (list) => `
   <li
     data-value="${list.name}" 
-    class="list-group-item list-group-item-action d-flex align-items-center px-3"
+    class="list-group-item list-group-item-action d-flex align-items-center justify-content-start rounded-0 py-0 mb-1 rounded-1 active"
     id="${list.id}"
   >
-    <i class="bi bi-list fs-3"></i>
+    <i class="bi bi-list fs-3 px-2"></i>
+    <form>
+      <input 
+        type="text" 
+        value="${list.name}" 
+        class="form-control visually-hidden" id="${list.id}">
+    </form>
     <span class="">${list.name}</span>
   </li>
 `;
