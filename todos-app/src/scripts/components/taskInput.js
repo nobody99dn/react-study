df57940b0ab4937efdb0d76ec093728229862f44
bao.thai <bao.thai@asnet.com.vn>
2022-03-22 17:31:21 +0700

    Implement task input

---
+++ todos-app/src/scripts/components/taskInput.js
@@ -0,0 +1,11 @@
+const taskInput = () => `
+<form>
+  <div class="input-group rounded border mt-3">
+    <i class="add-icon bi bi-plus-lg p-2"></i>
+    <input type="text" class="form-control p-2" placeholder="Add a task">
+    <i class="calendar-icon bi bi-calendar-day p-2"></i>
+  </div>
+</form>
+`;
+
+export default taskInput;
