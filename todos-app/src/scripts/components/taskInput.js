aec601a2a62df938afd4b2d4ca393d522fd73e75
bao.thai <bao.thai@asnet.com.vn>
2022-03-22 10:51:49 +0700

    Implement task line component and add 1 theme color to basestyle css

--- todos-app/src/scripts/components/taskInput.js
+++ todos-app/src/scripts/components/taskInput.js
@@ -1,10 +1,1 @@
-const taskLine = () => `
-  <div class="taskDescription row">
-    <div class="checkbox"></div>
-    <div class="task-title"></div>
-    <div class=""></div>
-  </div>
-  <div class="task-subtitle"></div>
-`;
 
-export default taskLine;
--- todos-app/src/scripts/components/taskLine.js
+++ todos-app/src/scripts/components/taskLine.js
@@ -1,5 +1,11 @@
-const taskLine = () => `
-  
+const taskLine = ($icon = '', $text = '', $radioId = '') => `
+  <div class="taskDescription p-3">
+    <div class="task-line border border-1 p-3 rounded mb-1">
+      <input class="form-check-input" type="radio" id="${$radioId}">
+      <label class="form-check-label px-3">${$text}</label>
+      <div class="task-icon float-end">${$radioId} </div>
+    </div>
+  </div>
 `;
 
 export default taskLine;
