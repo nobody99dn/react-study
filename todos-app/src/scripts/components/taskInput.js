74107a0d064c3df5590361b361b8dcd802fca711
Van Tran <van.tran@asnet.com.vn>
2022-03-23 15:50:19 +0700

    Update style task input

--- todos-app/src/scripts/components/taskInput.js
+++ todos-app/src/scripts/components/taskInput.js
@@ -1,5 +1,5 @@
 const taskInput = () => `
-<form onsubmit="submitTask()">
+<form class="task-form" onsubmit="submitTask()">
   <div class="input-group rounded border mt-3">
       <i class="add-icon bi bi-plus-lg p-2"></i>
       <input type="text" class="form-control p-2" placeholder="Add a task">
