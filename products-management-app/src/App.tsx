478a8c019085f742b3c095b72056621beecf6b90
Van Tran <van.tran@asnet.com.vn>
2022-06-01 22:44:21 +0700

    Implement structure

---
+++ products-management-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './reset.css';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      Product Management Application
+    </div>
+  );
+}
+
+export default App;
