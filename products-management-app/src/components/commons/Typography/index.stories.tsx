97d3ed8e45e5548430e8b6370f001331bb06fef0
Van Tran <van.tran@asnet.com.vn>
2022-06-02 10:46:29 +0700

    Implement Typography component

---
+++ products-management-app/src/components/commons/Typography/index.stories.tsx
@@ -0,0 +1,37 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Typography, VariantsTypes } from './index';
+
+import '../../../variables.css';
+import '../../../App.css';
+
+export default {
+  title: 'Components/Typography',
+  component: Typography
+} as ComponentMeta<typeof Typography>;
+
+const Template: ComponentStory<typeof Typography> = (args) => <Typography {...args} />;
+
+export const Text = Template.bind({});
+Text.args = {
+  children: 'Typography',
+};
+
+export const Highlight = Template.bind({});
+Highlight.args = {
+  children: 'Typography',
+  variant: VariantsTypes.Highlight
+};
+
+export const Title = Template.bind({});
+Title.args = {
+  children: 'Typography',
+  variant: VariantsTypes.Title
+};
+
+export const Subtitle = Template.bind({});
+Subtitle.args = {
+  children: 'Typography',
+  variant: VariantsTypes.Subtitle
+};
+
