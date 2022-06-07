e2c50b6381b4f10079bb0bb3ebbe30ded8ec005a
Van Tran <van.tran@asnet.com.vn>
2022-06-08 00:50:01 +0700

    Implement Modal component

--- products-management-app/src/components/Modal/index.stories.tsx
+++ products-management-app/src/components/Modal/index.stories.tsx
@@ -1,19 +1,17 @@
 // Libraries
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-// Components
+// Component
 import { Modal } from './index';
+import { Form } from '@components/Form';
 
 // Styles
 import '@assets/styles/App.css';
 import '@assets/styles/variables.css';
 import './index.css';
 
-// Images
-import logo from '@assets/images/logo.png';
-import iPhoneImage from '@assets/images/iphone-13-promax-alpine-green-alpine-green-color.jpg';
-import deleteIcon from '@assets/images/icons/trash-fill.svg';
-import { Currencies, ModalVariants } from '@/constants/types';
+// Constant
+import { FormVariants } from '@/constants/types';
 
 export default {
   title: 'Components/Modal',
@@ -25,5 +23,15 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   show: true,
-  variant: ModalVariants.Create
+  children:
+    <Form
+      handleSubmit={() => { }}
+      variants={FormVariants.Create}
+      productItem={{
+        id: '',
+        name: '',
+        type: '',
+        price: 0
+      }}
+    />
 };
