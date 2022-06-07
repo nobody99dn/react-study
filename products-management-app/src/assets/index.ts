a0b7fcfd711a7c25784af49c6642045299974eb1
Van Tran <van.tran@asnet.com.vn>
2022-06-08 00:50:01 +0700

    Implement Modal component

--- products-management-app/src/assets/index.ts
+++ products-management-app/src/assets/index.ts
@@ -3,12 +3,14 @@ Images
 ----------*/
 import trashFillIcon from './images/icons/trash-fill.svg'
 import searchIcon from './images/icons/search.svg'
+import closeIcon from './images/icons/x-lg.svg'
 import logo from './images/logo.png'
 import iPhoneImage from './images/iphone-13-promax-alpine-green-alpine-green-color.jpg'
 
 export {
   trashFillIcon,
   searchIcon,
+  closeIcon,
   logo,
   iPhoneImage
 }
