c04526296b29b30a51736b90dea4e0c2786db8ff
Van Tran <van.tran@asnet.com.vn>
2022-06-07 10:43:51 +0700

    Implement Select component

---
+++ products-management-app/src/components/commons/Select/index.stories.tsx
@@ -0,0 +1,30 @@
+// Libraries
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+// Components
+import { Select } from './index';
+
+// Styles
+import '@assets/styles/reset.css';
+import '@assets/styles/App.css';
+import '@assets/styles/variables.css';
+
+// Icons
+import { searchIcon } from '@/assets';
+
+// Constants
+import { PRODUCT_TYPES, TextFieldVariants } from '@/constants/types';
+
+export default {
+  title: 'Components/Select',
+  component: Select
+} as ComponentMeta<typeof Select>;
+
+const Template: ComponentStory<typeof Select> = (args) => <Select {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Select label',
+  name: 'select',
+  options: PRODUCT_TYPES
+};
