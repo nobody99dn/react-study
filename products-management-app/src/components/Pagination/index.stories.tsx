5167a0e06789aa893e6ebf2c14f68485f21f2665
Van Tran <van.tran@asnet.com.vn>
2022-06-07 14:10:36 +0700

    Implement Pagination component

---
+++ products-management-app/src/components/Pagination/index.stories.tsx
@@ -0,0 +1,24 @@
+// Libraries
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+// Components
+import { Pagination } from './index';
+
+// Styles
+import '@assets/styles/reset.css';
+import '@assets/styles/App.css';
+import '@assets/styles/variables.css';
+
+export default {
+  title: 'Components/Pagination',
+  component: Pagination
+} as ComponentMeta<typeof Pagination>;
+
+const Template: ComponentStory<typeof Pagination> = (args) => <Pagination {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  postsPerPage: 6,
+  totalPosts: 37,
+  paginate: () => { }
+};
---
+++ products-management-app/src/components/Pagination/index.tsx
@@ -0,0 +1,50 @@
+// Library
+import React, { useState } from "react";
+
+// Styles
+import './index.css';
+
+interface PaginationProps {
+  postsPerPage: number;
+  totalPosts: number;
+  paginate: (pageOrder: number) => void;
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  postsPerPage,
+  totalPosts,
+  paginate
+}) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageNumbers: number[] = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handleClick = (number: number) => {
+    setCurrentPage(number);
+
+    paginate(number);
+  };
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        {pageNumbers.map((number: number) => (
+          <li
+            key={number}
+            className={
+              [
+                'page-item', `${(currentPage === number) && 'active'}`
+              ].join(' ')
+            }
+            onClick={() => handleClick(number)}
+          >
+            {number}
+          </li>
+        ))}
+      </ul>
+    </nav >
+  );
+};
