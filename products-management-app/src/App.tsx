4176e5748055db5abfc90e8ccef6d63343a5274d
Van Tran <van.tran@asnet.com.vn>
2022-06-08 10:28:21 +0700

    Implement Main layout

--- products-management-app/src/App.tsx
+++ products-management-app/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './assets/styles/reset.css';
 import './assets/styles/App.css';
+import './assets/styles/variables.css';
+
+import Main from '@/layouts/Main';
 
 function App() {
 
   return (
-    <p>
-      Product Management Application
-    </p>
+    <Main />
   );
 }
 
