9cdfac44617e5cf7b9efe9f19505eefaf4983bb8
Van Tran <van.tran@asnet.com.vn>
2022-07-07 09:01:29 +0700

    Update missing import

--- products-management-app/src/components/commons/Text/index.tsx
+++ products-management-app/src/components/commons/Text/index.tsx
@@ -1,36 +1,34 @@
 // Library
-import { memo, ReactNode } from "react";
+import { memo, ReactNode } from 'react';
 
 // Constants
-import { FwType } from '@constants/types';
+import { FwType } from '@constants/index';
 
 // Styles
 import './index.css';
 
 export enum VariantsTypes {
   Default = 'default',
-  Highlight = 'highlight',
+  Highlight = 'highlight'
 }
 
 interface TextProps {
   children: ReactNode;
   color?: string;
   fs?: 'inherit' | 'italic';
-  fw?: FwType,
+  fw?: FwType;
   size?: string;
   variant?: VariantsTypes;
 }
 
-const Text: React.FC<TextProps> = (
-  {
-    children,
-    color,
-    fw = FwType.Normal,
-    size,
-    fs = 'inherit',
-    variant = VariantsTypes.Default,
-  }
-) => (
+const Text: React.FC<TextProps> = ({
+  children,
+  color,
+  fw = FwType.Normal,
+  size,
+  fs = 'inherit',
+  variant = VariantsTypes.Default
+}) => (
   <p
     className={`text-${variant}`}
     style={{
