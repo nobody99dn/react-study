079a394e528daf9850cf139cbcbdcf47f5b5d2d0
Van Tran <van.tran@asnet.com.vn>
2022-07-14 14:49:52 +0700

    Implement unit test for Text component

--- practice-3/products-management-app/src/components/commons/Text/index.test.tsx
+++ practice-3/products-management-app/src/components/commons/Text/index.test.tsx
@@ -1,7 +1,7 @@
 // Libraries
-import { render, act, fireEvent, cleanup } from '@testing-library/react';
-import { screen } from '@testing-library/dom';
+import { render, cleanup } from '@testing-library/react';
 import { create } from 'react-test-renderer';
+import '@testing-library/jest-dom';
 
 // Components
 import Text, { VariantsTypes } from './index';
@@ -25,18 +25,28 @@ describe('Button component', () => {
   });
 
   test('should render correctly', () => {
-    const defaultText = create(<Text>Typography</Text>).toJSON();
+    const defaultText = create(<Text>text</Text>).toJSON();
     expect(defaultText).toMatchSnapshot();
 
     const highlightText = create(
       <Text variant={VariantsTypes.Highlight}>Typography</Text>
-    );
+    ).toJSON();
     expect(highlightText).toMatchSnapshot();
 
-    const boldText = create(<Text fw={FwType.Bold}>Typography</Text>);
+    const boldText = create(<Text fw={FwType.Bold}>Typography</Text>).toJSON();
     expect(boldText).toMatchSnapshot();
 
-    const italicText = create(<Text fs={FsType.Italic}>Typography</Text>);
+    const italicText = create(
+      <Text fs={FsType.Italic}>Typography</Text>
+    ).toJSON();
     expect(italicText).toMatchSnapshot();
+  });
+
+  test('should render Text with content is Typography', () => {
+    const text = 'Typography';
+
+    const { getByText } = render(<Text>{text}</Text>, { container });
+
+    expect(getByText(text).textContent).toMatch(text);
   });
 });
