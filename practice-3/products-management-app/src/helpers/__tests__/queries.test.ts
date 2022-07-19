34cc9b3b006aa717bd70a9a6f1bcc6738f0f6707
Van Tran <van.tran@asnet.com.vn>
2022-07-19 09:03:01 +0700

    Implement test for queries

--- practice-3/products-management-app/src/helpers/__tests__/queries.test.ts
+++ practice-3/products-management-app/src/helpers/__tests__/queries.test.ts
@@ -1,5 +1,19 @@
+import { ProductTypes } from '@constants/types';
 import { filterTypeAndPriceOrder, queryProducts } from '..';
+import { FilterOrderOptions } from '@constants/types';
 
 describe('Query testing', () => {
-  test('should return correctly', () => {});
+  test('should return correctly', () => {
+    expect(
+      filterTypeAndPriceOrder(ProductTypes.Laptop, '' as FilterOrderOptions)
+    ).toMatch('?type=Laptop');
+
+    expect(
+      filterTypeAndPriceOrder('' as ProductTypes, FilterOrderOptions.Desc)
+    ).toMatch('sortBy=price&order=desc');
+
+    expect(
+      filterTypeAndPriceOrder(ProductTypes.Phone, FilterOrderOptions.Desc)
+    ).toMatch('?type=Phone&sortBy=price&order=desc');
+  });
 });
