d3dce625ce35284fb90f242e2b90d285127ca9cd
Van Tran <van.tran@asnet.com.vn>
2022-07-19 11:00:00 +0700

    Implement testing api

--- practice-3/products-management-app/src/helpers/__tests__/clientRequests.test.ts
+++ practice-3/products-management-app/src/helpers/__tests__/clientRequests.test.ts
@@ -1,17 +1,116 @@
+// Libraries
+import fetch from 'jest-fetch-mock';
+import axios from 'axios';
+
+// Requests
 import { get, post, update, remove } from '..';
 
-import fetch from 'jest-fetch-mock';
+// Constant
+import { ProductTypes } from '@constants/types';
+
+// Product
+import { Product } from '@models/product';
+
+jest.mock('axios');
 
 describe('Testing API', () => {
   beforeEach(() => {
     fetch.resetMocks();
   });
 
-  test('should load product data', () => {});
+  test('fetches successfully data from an API', async () => {
+    const mockData: Product[] = [
+      {
+        id: '1',
+        imageUrl: '',
+        name: 'Product test 1',
+        price: 1000000,
+        type: ProductTypes.Phone
+      },
+      {
+        id: '2',
+        imageUrl: '',
+        name: 'Product test 2',
+        price: 2000000,
+        type: ProductTypes.Laptop
+      },
+      {
+        id: '3',
+        imageUrl: '',
+        name: 'Product test 3',
+        price: 3000000,
+        type: ProductTypes.Tablet
+      }
+    ];
+    (axios.get as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve({ data: mockData })
+    );
 
-  test('should create data', () => {});
+    const res: Product[] = await get('url');
 
-  test('should update data', () => {});
+    expect(res).toEqual(mockData);
+  });
 
-  test('should should remove data', () => {});
+  test('fetches erroneously data from an API', async () => {
+    const errorMessage = 'Network Error';
+
+    (axios.get as jest.Mock).mockImplementationOnce(() =>
+      Promise.reject(new Error(errorMessage))
+    );
+
+    await expect(get('url')).rejects.toThrow(errorMessage);
+  });
+
+  test('should create data', async () => {
+    const mockData: Product = {
+      id: '1',
+      imageUrl: '',
+      name: 'Product test 1',
+      price: 1000000,
+      type: ProductTypes.Phone
+    };
+
+    (axios.post as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve({ data: mockData })
+    );
+
+    const res: Product = await post('url', mockData);
+
+    expect(res).toEqual(mockData);
+  });
+
+  test('should update data', async () => {
+    const updateData: Product = {
+      id: '1',
+      imageUrl: '',
+      name: 'Product test 1',
+      price: 2000000,
+      type: ProductTypes.Phone
+    };
+
+    (axios.put as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve({ data: updateData })
+    );
+
+    const updatedData: Product = await update('url', updateData);
+    expect(updatedData).toEqual(updateData);
+  });
+
+  test('should remove data', async () => {
+    const mockData: Product = {
+      id: '1',
+      imageUrl: '',
+      name: 'Product test 1',
+      price: 1000000,
+      type: ProductTypes.Phone
+    };
+
+    (axios.delete as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve({ data: mockData })
+    );
+
+    const res: Product = await remove('url');
+
+    expect(res).toEqual(mockData);
+  });
 });
