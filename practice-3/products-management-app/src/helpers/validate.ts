51983631a9cff968c3751527e75e1bbaf1c212cf
Van Tran <van.tran@asnet.com.vn>
2022-07-24 19:48:27 +0700

    Update test coverage

--- practice-3/products-management-app/src/helpers/validate.ts
+++ practice-3/products-management-app/src/helpers/validate.ts
@@ -1,4 +1,5 @@
 // Constants
+import { ERROR_MESSAGES } from '@constants/messages';
 import { INIT_ERRORS, ProductTypes, ValidateError } from '@constants/types';
 
 // Model
@@ -8,20 +9,20 @@ export const validateProduct = ({ name, type, price, imageUrl }: Product) => {
   const errors: ValidateError = { ...INIT_ERRORS };
 
   if (!name) {
-    errors.name = 'Username is required!';
+    errors.name = ERROR_MESSAGES.PRODUCT_NAME_REQUIRED;
   }
   if (!type) {
-    errors.type = 'Type is required!';
+    errors.type = ERROR_MESSAGES.PRODUCT_TYPE_REQUIRED;
   } else if (
     !Object.values(ProductTypes).includes(type as unknown as ProductTypes)
   ) {
     errors.type = `${type} is exists!`;
   }
   if (!price) {
-    errors.price = 'Price is required!';
+    errors.price = ERROR_MESSAGES.PRODUCT_PRICE_REQUIRED;
   }
   if (!imageUrl) {
-    errors.imageUrl = 'Image is required!';
+    errors.imageUrl = ERROR_MESSAGES.PRODUCT_IMAGE_REQUIRED;
   }
 
   return errors;
