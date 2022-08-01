ef9b08bd09c4a5e02e0554a285cd713a362c4f89
Van Tran <van.tran@asnet.com.vn>
2022-08-01 10:56:43 +0700

    Implement mdx file

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: []
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  }
+});
+
+const nextConfig = withMDX({
   reactStrictMode: true,
-  swcMinify: true
-};
+  swcMinify: true,
+
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+});
 
 module.exports = nextConfig;
