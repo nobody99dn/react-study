2c33547f90cc8f96eeceb8f4e946a051692c52f7
Van Tran <van.tran@asnet.com.vn>
2022-08-15 17:50:36 +0700

    Implement Error Boudary

--- movie-box-app/src/pages/_app.tsx
+++ movie-box-app/src/pages/_app.tsx
@@ -7,10 +7,14 @@ import 'tailwindcss/tailwind.css';
 
 // Layouts
 import Layout from '@layouts/Main';
+import ErrorBoundary from '@components/ErrorBoudary';
+import LoadingIndicator from '@components/LoadingIndicator';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Layout>
-    <Component {...pageProps} />
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
   </Layout>
 );
 
