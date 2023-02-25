dca7c6afbefc0ea4d930c7ea8454132dc64e3618
vawn <vantran99dn@gmail.com>
2023-02-25 18:49:42 +0700

    Implement constructor

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine setup
+app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'main' }));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resources/views'));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/news', (req, res) => res.render('news'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
